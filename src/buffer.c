6cc4828f35c6c6fd037d2bb42ff1fde61a79fa68
wwotz <lukeanthonyholland@gmail.com>
2023-04-04 20:02:28 +0100

    updated buffer.c for more functionality.

--- src/buffer.c
+++ src/buffer.c
@@ -19,6 +19,24 @@ int sdfe_buffer_init(sdfe_buffer_t *buffer, const char *data)
         return sdfe_buffer_write(buffer, data);
 }
 
+int sdfe_buffer_append(sdfe_buffer_t *buffer, char c)
+{
+        if (!buffer) {
+                SDFE_PUSH_ERROR("buffer_t structure was null!");
+                return SDFE_BUFFER_NULL_ERROR;
+        }
+
+        if (buffer->capacity <= buffer->size + 1) {
+                int status;
+                if ((status = sdfe_buffer_resize(buffer)) != SDFE_BUFFER_NO_ERROR)
+                        return status;
+        }
+
+        buffer->data[buffer->size++] = c;
+        buffer->data[buffer->size] = '\0';
+        return SDFE_BUFFER_NO_ERROR;
+}
+
 int sdfe_buffer_write(sdfe_buffer_t *buffer, const char *data)
 {
         return sdfe_buffer_insert(buffer, data, 0);
@@ -44,11 +62,41 @@ int sdfe_buffer_insert(sdfe_buffer_t *buffer, const char *data, size_t offset)
         return SDFE_BUFFER_NO_ERROR;
 }
 
+int sdfe_buffer_clear(sdfe_buffer_t *buffer)
+{
+        if (!buffer) {
+                SDFE_PUSH_ERROR("buffer_t structure was null!");
+                return SDFE_BUFFER_NULL_ERROR;
+        }
+
+        if (!buffer->data) {
+                SDFE_PUSH_ERROR("buffer_t data was null!");
+                return SDFE_BUFFER_DATA_ERROR;
+        }
+
+        buffer->size = 0;
+        memset(buffer->data, 0, buffer->capacity);
+        return SDFE_BUFFER_NO_ERROR;
+}
+
 int sdfe_buffer_length(sdfe_buffer_t *buffer)
 {
         if (buffer)
                 return buffer->size;
         return 0;
+}
+
+char sdfe_buffer_get_charat(sdfe_buffer_t *buffer, int index)
+{
+        if (buffer && index < sdfe_buffer_length(buffer))
+                return buffer->data[index];
+        return '\0';
+}
+
+void sdfe_buffer_set_charat(sdfe_buffer_t *buffer, int index, char c)
+{
+        if (buffer && index < sdfe_buffer_length(buffer))
+                buffer->data[index] = c;
 }
 
 int sdfe_buffer_resize(sdfe_buffer_t *buffer)
